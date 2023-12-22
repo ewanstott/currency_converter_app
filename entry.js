9a0cc332f9d38a8b74e190b9bc35775e0bfe21d2
Ewan Stott <ewan.stott1@gmail.com>
2023-12-22 12:59:30 +0000

    first commit

---
+++ entry.js
@@ -0,0 +1,9 @@
+//get api data that shows the conversion per currency
+
+//create an interface that allows entry of:
+//start value
+//destination currency
+
+//do the maths
+
+//display it to the DOM
